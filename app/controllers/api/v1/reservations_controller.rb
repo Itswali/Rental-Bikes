9212cf0fa4caa9e855d2fa55db7a18a3779c6fd6
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-17 23:44:01 +0530

    updated reservation model

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,6 +1,35 @@
 module Api
     module V1
         class ReservationsController < ApplicationController
+            protect_from_forgery with: :null_session
+
+            def index
+                reservations = Reservation.all
+                render json: ReservationSerializer.new(reservations).serialized_json
+            end
+
+            def create
+                reservation = Reservation.new(reservation_params)
+                if reservation.save
+                render json: ReservationSerializer.new(reservation).serialized_json
+                else
+                    render json: {errors: reservation.errors.full_messages}, status: 422
+                end               
+            end
+
+            def destroy
+             reservation = Reservation.find(params[:id])
+             if reservation.destroy
+                head :no_content
+                else
+                render json: {errors: reservation.errors.full_messages}, status: 422
+
+                end
+            end
+        private
+         def reservation_params
+            params.require(:reservation).permit(:Date, :city, :user_id, :item_id)
+         end
 
         end
     end
