52af9ae47a27deba3f156422bd3ee24ebd520ec5
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-18 12:14:43 +0530

    updated controllers

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class SessionsController < ApplicationController
+        def create
+          user = User.find_by(email: params[:email])
+          
+          if user&.valid_password?(params[:password])
+            session[:user_id] = user.id
+            render json: { message: 'Logged in successfully' }
+          else
+            render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
+          end
+        end
+      
+        def destroy
+          session.delete(:user_id)
+          render json: { message: 'Logged out successfully' }
+        end
+      end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # Defines the root path route ("/")
     namespace :api do
      namespace :v1 do
