ad61fe08c299f144b601cf965befc56791a03513
Itswali <luckywali0986@gmail.com>
2023-08-19 14:09:30 +0500

    fix rubocop error

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -1,31 +1,12 @@
 module Api
   module V1
     class RegistrationsController < ApplicationController
-<<<<<<< HEAD
-            def create
-              user = User.new(registration_params)
-          
-              if user.save
-                session[:user_id] = user.id
-                render json: { message: 'User registered successfully', user: user }
-              else
-                render json: { errors: user.errors.full_messages }, 
-                status: :unprocessable_entity
-              end
-            end
-          
-            private
-          
-            def registration_params
-              params.permit(:email, :password)
-      end
-=======
       def create
         user = User.new(registration_params)
 
         if user.save
           session[:user_id] = user.id
-          render json: { message: 'User registered successfully' }
+          render json: { message: 'User registered successfully', user: }
         else
           render json: { errors: user.errors.full_messages },
                  status: :unprocessable_entity
@@ -35,9 +16,8 @@ module Api
       private
 
       def registration_params
-        params.permit(:email, :password, :password_confirmation)
+        params.permit(:email, :password)
       end
->>>>>>> b6e07e4dfd23b1ea0ea49884af9f86b3870ec5af
     end
   end
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,29 +1,12 @@
 module Api
   module V1
     class SessionsController < ApplicationController
-<<<<<<< HEAD
-            def create
-              user = User.find_by(email: params[:email])
-              
-              if user&.valid_password?(params[:password])
-                session[:user_id] = user.id
-                render json: { message: 'Logged in successfully', user: user }
-              else
-                render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
-              end
-            end
-          
-            def destroy
-              session.delete(:user_id)
-              render json: { message: 'Logged out successfully' }
-      end
-=======
       def create
         user = User.find_by(email: params[:email])
 
         if user&.valid_password?(params[:password])
           session[:user_id] = user.id
-          render json: { message: 'Logged in successfully' }
+          render json: { message: 'Logged in successfully', user: }
         else
           render json: { error: 'Invalid email or password' }, status: :unprocessable_entity
         end
@@ -33,7 +16,6 @@ module Api
         session.delete(:user_id)
         render json: { message: 'Logged out successfully' }
       end
->>>>>>> b6e07e4dfd23b1ea0ea49884af9f86b3870ec5af
     end
   end
 end
