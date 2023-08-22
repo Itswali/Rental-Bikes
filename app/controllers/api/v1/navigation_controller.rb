dff10d75904884c947a40d843996bcc86ef3b783
Itswali <luckywali0986@gmail.com>
2023-08-22 16:26:55 +0500

    create delte feature button

--- app/controllers/api/v1/navigation_controller.rb
+++ app/controllers/api/v1/navigation_controller.rb
@@ -3,11 +3,11 @@ module Api
     class NavigationController < ApplicationController
       def links
         navigation_links = [
-          { name: 'Cars', link: '/cars' },
+          # { name: 'Cars', link: '/cars' },
           { name: 'Reserve', link: '/reserve' },
           { name: 'My Reservations', link: '/my_reservations' },
-          { name: 'Add Item', link: '/add_item' },
-          { name: 'Delete Item', link: '/delete_item' }
+          { name: 'Add Item', link: '/add_item' }
+          # { name: 'Delete Item', link: '/delete_item' }
         ]
         render json: navigation_links
       end
