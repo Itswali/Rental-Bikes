771b223d2aa64fd5ba2c2134dcedbb5ef87d9b03
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-24 17:41:00 +0530

    merged navigation controller

---
+++ app/controllers/api/v1/navigation_controller.rb
@@ -0,0 +1,15 @@
+module Api
+  module V1
+    class NavigationController < ApplicationController
+      def links
+        navigation_links = [
+          # { name: 'Cars', link: '/cars' },
+          { name: 'Reserve', link: '/reserve' },
+          { name: 'My Reservations', link: '/my_reservations' },
+          { name: 'Add Item', link: '/add_item' }
+          # { name: 'Delete Item', link: '/delete_item' }
+        ]
+        render json: navigation_links
+      end
+    end
+  end
