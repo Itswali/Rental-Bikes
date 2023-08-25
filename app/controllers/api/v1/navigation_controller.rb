5880400cd7d351983412d0593921e49fa95a4104
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-08-25 15:04:18 +0530

    pulled latest pull

--- app/controllers/api/v1/navigation_controller.rb
+++ app/controllers/api/v1/navigation_controller.rb
@@ -6,7 +6,9 @@ module Api
           # { name: 'Cars', link: '/cars' },
           { name: 'Reserve', link: '/reserve' },
           { name: 'My Reservations', link: '/my_reservations' },
-          { name: 'Add Item', link: '/add_item' }
+          { name: 'Add Item', link: '/add_item' },
+          { name: 'Sign Out', link: '/logout' }
+
           # { name: 'Delete Item', link: '/delete_item' }
         ]
         render json: navigation_links
