f381cbcbb796bd503506fae990e897d854f6d8ad
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-25 10:33:18 +0000

    Fix issues after merging

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-    namespace :api do
-     namespace :v1 do
-       resources :items
-       resources :reservations
-       post '/login', to: 'sessions#create'
-       post '/signup', to: 'registrations#create'
-       delete '/logout', to: 'sessions#destroy'
-       get '/navigation_links', to: 'navigation#links' # New route for navigation links
-     end
+  namespace :api do
+    namespace :v1 do
+      resources :items
+      resources :reservations
+      post '/login', to: 'sessions#create'
+      post '/signup', to: 'registrations#create'
+      delete '/logout', to: 'sessions#destroy'
+      get '/navigation_links', to: 'navigation#links' # New route for navigation links
+    end
+  end
 end
