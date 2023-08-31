b3b462f8df18332780e225f8c34ac1b802affc8f
Samuel Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-08-31 19:07:10 +0000

    Update cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'  # front-end URL
+      origins 'https://www.google.com'  # front-end URL
       resource '*',
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
