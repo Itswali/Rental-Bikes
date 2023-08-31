942af9d6c49b2393f5ab477a7d2d60a098b6f160
Riyaz Ahmed <r-ahmed@riysaim.com>
2023-09-01 01:16:34 +0530

    updated resservations_controller

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         credentials: true
     end
     allow do
-        origins 'https://rental-rkib.onrender.com/'
+        origins 'https://supercarsrentals.onrender.com'
         resource '*',
           headers: :any,
           methods: [:get, :post, :put, :patch, :delete, :options, :head],
