0e8edf4bbbcbbc65d7ce3e22815cd136ede49ea3
Itswali <luckywali0986@gmail.com>
2023-09-01 11:20:56 +0500

    fix user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
 
   has_many :items, dependent: :destroy
   has_many :reservations, dependent: :destroy, class_name: 'Reservation'
-  # has_many :items
   has_many :reservations
   validates :email, presence: true, uniqueness: true
 end
